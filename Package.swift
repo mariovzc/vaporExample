9067a13a8b10d2e8d6d68b6d69f791f3403801fd
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-03-13 14:24:31 -0500

    added vapor dependence

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "vaporExample"
+    name: "vaporExample",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+    ]
 )
