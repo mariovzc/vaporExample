5557d6dabd64f155bb33fc6e0eeb2addcd48dece
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-03-14 07:59:55 -0500

    some things for the controller

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import Vapor
+import HTTP
+
+let drop = Droplet()
+
+drop.get("/") { _ in
+  return "Hello Vapor"
+}
+drop.get("user", ":id"){ request in
+  guard let userId = request.parameters["id"]?.int else {
+    throw Abort.badRequest
+  }
+  return try JSON(node: [
+        "number": userId,
+        "text": "unicorns",
+        "bool": false
+    ])
+}
+drop.get("products"){ request in
+    let products = try drop.client.get("http://apiror.herokuapp.com/products.json")
+    return products
+}
+drop.run()
